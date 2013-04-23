ea67e7b6dafbc7b7f1a816c96ff3f375c49cfd6b
Matthew Nielsen <xunker@pyxidis.org>
2013-04-22 18:12:18 -0600

    Allow height, width and depth to be optional on Pieces.

--- lib/dhl/get_quote/piece.rb
+++ lib/dhl/get_quote/piece.rb
@@ -1,34 +1,59 @@
 class Dhl::GetQuote::Piece
   attr_accessor :piece_id
-  REQUIRED = [ :height, :weight, :width, :depth ]
+
   def initialize(options = {})
-    REQUIRED.each do |req|
-      if options[req].to_i > 0
-        instance_variable_set("@#{req}", options[req].to_i)
-      else
-        raise Dhl::GetQuote::OptionsError, ":#{req} is a required for Dhl::GetQuote::Piece. Must be nonzero integer."
-      end
-      @piece_id = options[:piece_id] || 1
+    [ :width, :height, :depth, :weight ].each do |i|
+      options[i] = options[i].to_i if !!options[i]
     end
+
+    if options[:weight] && options[:weight] > 0
+      @weight = options[:weight]
+    else
+      raise Dhl::GetQuote::OptionsError, required_option_error_message(:weight)
+    end
+
+    if options[:width] || options[:height] || options[:depth]
+      [ :width, :height, :depth ].each do |req|
+        if options[req].to_i > 0
+          instance_variable_set("@#{req}", options[req].to_i)
+        else
+          raise Dhl::GetQuote::OptionsError, required_option_error_message(req)
+        end
+      end
+    end
+
+    @piece_id = options[:piece_id] || 1
+
   end
 
   def to_h
     h = {}
-    REQUIRED.each do |req|
-      h[req.to_s.capitalize] = instance_variable_get("@#{req}")
+    [ :width, :height, :depth, :weight ].each do |req|
+      if x = instance_variable_get("@#{req}")
+        h[req.to_s.capitalize] = x
+      end
     end
     h
   end
 
   def to_xml
-<<eos
+    xml_str = <<eos
 <Piece>
   <PieceID>#{@piece_id}</PieceID>
-  <Height>#{@height}</Height>
-  <Depth>#{@depth}</Depth>
-  <Width>#{@width}</Width>
-  <Weight>#{@weight}</Weight>
-</Piece>
 eos
+
+    xml_str << "  <Height>#{@height}</Height>\n" if @height
+    xml_str << "  <Depth>#{@depth}</Depth>\n" if @depth
+    xml_str << "  <Width>#{@width}</Width>\n" if @width
+    xml_str << "  <Weight>#{@weight}</Weight>\n" if @weight
+
+    xml_str += "</Piece>\n"
+    xml_str
+  end
+
+private
+
+  def required_option_error_message(field)
+    ":#{field} is a required for Dhl::GetQuote::Piece. Must be nonzero integer."
   end
 end
--- spec/lib/dhl/get_quote/piece_spec.rb
+++ spec/lib/dhl/get_quote/piece_spec.rb
@@ -18,30 +18,42 @@ describe Dhl::GetQuote::Piece do
       klass.new(valid_params).must be_an_instance_of(klass)
     end
 
-    it "must throw an error if a height is not passed" do
-      lambda do
-        klass.new(valid_params.merge(:height => nil))
-      end.must raise_exception(Dhl::GetQuote::OptionsError)
-    end
-
     it "must throw an error if a weight is not passed" do
       lambda do
         klass.new(valid_params.merge(:weight => nil))
       end.must raise_exception(Dhl::GetQuote::OptionsError)
     end
 
-    it "must throw an error if a width is not passed" do
+    it "must allow only weight to be passed" do
+      klass.new( { :weight => 1 } ).must be_an_instance_of(klass)
+    end
+
+    it "must throw an error if a height is passed but width or depth is not" do
       lambda do
         klass.new(valid_params.merge(:width => nil))
       end.must raise_exception(Dhl::GetQuote::OptionsError)
-    end
-
-    it "must throw an error if a depth is not passed" do
       lambda do
         klass.new(valid_params.merge(:depth => nil))
       end.must raise_exception(Dhl::GetQuote::OptionsError)
     end
 
+    it "must throw an error if a width is passed but height or depth is not" do
+      lambda do
+        klass.new(valid_params.merge(:height => nil))
+      end.must raise_exception(Dhl::GetQuote::OptionsError)
+      lambda do
+        klass.new(valid_params.merge(:depth => nil))
+      end.must raise_exception(Dhl::GetQuote::OptionsError)
+    end
+
+    it "must throw an error if a depth is passed but height or width is not" do
+      lambda do
+        klass.new(valid_params.merge(:height => nil))
+      end.must raise_exception(Dhl::GetQuote::OptionsError)
+      lambda do
+        klass.new(valid_params.merge(:width => nil))
+      end.must raise_exception(Dhl::GetQuote::OptionsError)
+    end
   end
 
   describe "#to_h" do
@@ -64,6 +76,27 @@ describe Dhl::GetQuote::Piece do
   <Depth>4</Depth>
   <Width>3</Width>
   <Weight>2</Weight>
+</Piece>
+eos
+    end
+
+    it "must generate a xml with only weight and PieceID" do
+      dhl = klass.new( { :weight => 99 } )
+      dhl.to_xml.must == <<eos
+<Piece>
+  <PieceID>1</PieceID>
+  <Weight>99</Weight>
+</Piece>
+eos
+    end
+
+    it "must allow PieceID to be overridd" do
+      dhl = klass.new( { :weight => 1 } )
+      dhl.piece_id = 88
+      dhl.to_xml.must == <<eos
+<Piece>
+  <PieceID>88</PieceID>
+  <Weight>1</Weight>
 </Piece>
 eos
     end
